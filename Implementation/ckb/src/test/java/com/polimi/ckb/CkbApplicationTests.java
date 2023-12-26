fe1fe2d32c040efd953b98452705f8dc7ba3fcb9
lucca <simone.lucca@mail.polimi.it>
2023-12-26 18:08:17 +0100

    Add initial structure and dependencies for the project
    In this commit, the initial structure of the project has been set up with new files, such as application properties, java classes and tests. The pom.xml file has been added with the necessary dependencies for the project, including packages related to database, web, kafka, and testing. Unnecessary files regarding time tracking and UML sequence diagram have been removed.

---
+++ Implementation/ckb/src/test/java/com/polimi/ckb/CkbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polimi.ckb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
