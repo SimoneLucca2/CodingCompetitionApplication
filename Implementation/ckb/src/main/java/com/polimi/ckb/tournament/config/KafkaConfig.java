6ff0748ed0d5221eae029dfb62ec5036343ffad3
lucca <simone.lucca@mail.polimi.it>
2024-01-07 22:01:44 +0100

    Revise EducatorController and KafkaConfig; Update database URLs
    Expanded EducatorController to add educators, included new Kafka topics in KafkaConfig. The database URL in application.properties is updated to point to 'tournament_schema'. Also, the GenerationType in Tournament entity has been modified and server port has been explicitly set to 8080.

--- Implementation/ckb/src/main/java/com/polimi/ckb/tournament/config/KafkaConfig.java
+++ Implementation/ckb/src/main/java/com/polimi/ckb/tournament/config/KafkaConfig.java
@@ -7,9 +7,90 @@ import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
+
+    @Bean
+    public NewTopic battleStudentScoreTopic() {
+        return TopicBuilder.name("battle.student.score")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic emailSendingRequestTopic() {
+        return TopicBuilder.name("email.sending.request")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic inputTopicTopic() {
+        return TopicBuilder.name("inputTopic")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
     @Bean
     public NewTopic tournamentCreationTopic() {
         return TopicBuilder.name("tournament.creation")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic tournamentEducatorAddTopic() {
+        return TopicBuilder.name("tournament.educator.add")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic tournamentLifecycleActiveTopic() {
+        return TopicBuilder.name("tournament.lifecycle.active")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic tournamentLifecycleCloseTopic() {
+        return TopicBuilder.name("tournament.lifecycle.close")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic tournamentLifecycleClosingTopic() {
+        return TopicBuilder.name("tournament.lifecycle.closing")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic tournamentStudentJoinTopic() {
+        return TopicBuilder.name("tournament.student.join")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic tournamentStudentQuitTopic() {
+        return TopicBuilder.name("tournament.student.quit")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic userCreationTopic() {
+        return TopicBuilder.name("user.creation")
                 .partitions(1)
                 .replicas(1)
                 .build();
--- Implementation/ckb/src/main/java/com/polimi/ckb/tournament/controller/EducatorController.java
+++ Implementation/ckb/src/main/java/com/polimi/ckb/tournament/controller/EducatorController.java
@@ -21,12 +21,18 @@ public class EducatorController {
     private final EducatorService educatorService;
     private final AddEducatorKafkaProducer kafkaProducer;
 
+    /**
+     * Adds an educator to a tournament.
+     *
+     * @param msg the AddEducatorDto object containing the necessary information for adding an educator to a tournament
+     * @return a ResponseEntity containing the Educator object if the educator was successfully added, otherwise an error message
+     */
     @PostMapping
-    public ResponseEntity<?> addEducator(@Valid @RequestBody AddEducatorDto msg) {
+    public ResponseEntity<Object> addEducator(@Valid @RequestBody AddEducatorDto msg) {
         try {
             Educator response = educatorService.addEducatorToTournament(msg);
             kafkaProducer.sendAddedEducatorMessage(msg);
-            return ResponseEntity.ok(response);
+            return ResponseEntity.ok(response); //TODO debug
         }catch (Exception e) {
             return ResponseEntity.badRequest().body("Error: " + e.getMessage());
         }
