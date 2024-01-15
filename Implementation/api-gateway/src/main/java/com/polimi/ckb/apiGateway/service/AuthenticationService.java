6067e1ff49e537e4e1577fb869f6b0700b52adab
simonelucca2 <simone.lucca@mail.polimi.it>
2024-01-15 15:44:58 +0100

    Implement JWT authentication and user registration
    Created a comprehensive solution for JWT-based authentication, along with user registration functionality. New files and classes were added, including JwtService, User entity, UserRepository, and more. Additionally, updated the project's dependencies in pom.xml to support these new features.

---
+++ Implementation/api-gateway/src/main/java/com/polimi/ckb/apiGateway/service/AuthenticationService.java
@@ -0,0 +1,65 @@
+package com.polimi.ckb.apiGateway.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.polimi.ckb.apiGateway.dto.AuthenticationRequest;
+import com.polimi.ckb.apiGateway.dto.AuthenticationResponse;
+import com.polimi.ckb.apiGateway.dto.RegisterRequest;
+import com.polimi.ckb.apiGateway.entity.User;
+import com.polimi.ckb.apiGateway.repository.UserRepository;
+import com.polimi.ckb.apiGateway.utility.UserType;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .name(request.getName())
+                .surname(request.getSurname())
+                .nickname(request.getNickname())
+                .accountType(request.getAccountType())
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+}
