b40943e5f483b385c06f0913171309323d301e1b
simonelucca2 <simone.lucca@mail.polimi.it>
2024-01-17 14:18:40 +0100

    Refined security rules according to user roles
    Security configuration in SecurityConfig.java has been updated to handle request authorization based on the role of the user; namely 'EDUCATOR' and 'STUDENT'. This is done by creating RequestMatchers for educator and student specific endpoints, as well as for common ones, in order to streamline and enhance access control in the system.

--- Implementation/api-gateway/src/main/java/com/polimi/ckb/apiGateway/config/SecurityConfig.java
+++ Implementation/api-gateway/src/main/java/com/polimi/ckb/apiGateway/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.polimi.ckb.apiGateway.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import static org.springframework.http.HttpMethod.DELETE;
 import static org.springframework.http.HttpMethod.GET;
@@ -28,30 +32,57 @@ public class SecurityConfig {
     private static final String[] WHITE_LIST_URL = {
             "/login",
             "/signup",
-            "/**"
-    }; //todo
+    };
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // Define RequestMatchers for specific roles and endpoints
+        RequestMatcher educatorEndpoints = new OrRequestMatcher(
+                new AntPathRequestMatcher("/tournament/", HttpMethod.POST.name()),
+                new AntPathRequestMatcher("/tournament/educator", HttpMethod.POST.name()),
+                new AntPathRequestMatcher("/tournament/status", HttpMethod.PUT.name()),
+                new AntPathRequestMatcher("/battle", HttpMethod.POST.name()),
+                new AntPathRequestMatcher("/battle/score", HttpMethod.PUT.name())
+        );
+
+        RequestMatcher studentEndpoints = new OrRequestMatcher(
+                new AntPathRequestMatcher("/tournament/student", HttpMethod.POST.name()),
+                new AntPathRequestMatcher("/tournament/student", HttpMethod.DELETE.name()),
+                new AntPathRequestMatcher("/battle/student", HttpMethod.POST.name()),
+                new AntPathRequestMatcher("/battle/student", HttpMethod.DELETE.name()),
+                new AntPathRequestMatcher("/battle/group", HttpMethod.POST.name()),
+                new AntPathRequestMatcher("/battle/group", HttpMethod.PUT.name()),
+                new AntPathRequestMatcher("/battle/group", HttpMethod.DELETE.name())
+        );
+
+        RequestMatcher allUserEndpoints = new OrRequestMatcher(
+                new AntPathRequestMatcher("/tournament/ranking", HttpMethod.GET.name()),
+                new AntPathRequestMatcher("/tournament/", HttpMethod.GET.name()),
+                new AntPathRequestMatcher("/battle/score", HttpMethod.GET.name()),
+                new AntPathRequestMatcher("/user", HttpMethod.GET.name())
+        );
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req -> //todo
-                        req.requestMatchers(WHITE_LIST_URL)
-                                .permitAll()
-                                //.requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
-                                //.requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
-                                //.requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
-                                //.requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
-                                //.requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
-                                .anyRequest()
-                                .authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        // White list URLs
+                        .requestMatchers(WHITE_LIST_URL).permitAll()
+                        // Educator endpoints
+                        .requestMatchers(educatorEndpoints).hasAuthority("EDUCATOR")
+                        // Student endpoints
+                        .requestMatchers(studentEndpoints).hasAuthority("STUDENT")
+                        // Endpoints open for both Educator and Student
+                        .requestMatchers(allUserEndpoints).hasAnyAuthority("EDUCATOR", "STUDENT")
+                        // Any other requests must be authenticated
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-        ;
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
