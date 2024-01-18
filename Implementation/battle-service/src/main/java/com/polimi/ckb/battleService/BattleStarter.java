a78a13a5d7dfc2c7988d254ac56ad051f39f078e
MarcoF17 <marco1.filippini@mail.polimi.it>
2024-01-18 14:52:44 +0100

    http request from battle to tournament to get a json containing a tournament

--- Implementation/battle-service/src/main/java/com/polimi/ckb/battleService/BattleStarter.java
+++ Implementation/battle-service/src/main/java/com/polimi/ckb/battleService/BattleStarter.java
@@ -2,8 +2,10 @@ package com.polimi.ckb.battleService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BattleStarter {
     public static void main(String[] args) {
         SpringApplication.run(BattleStarter.class, args);
