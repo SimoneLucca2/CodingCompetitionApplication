99bd013115613883fabdfc1ecce7d8a83acff7ca
MarcoF17 <marco1.filippini@mail.polimi.it>
2024-01-28 19:23:38 +0100

    creation of sonarcloud project

--- Implementation/battle-service/src/main/java/com/polimi/ckb/battleService/service/GitService.java
+++ Implementation/battle-service/src/main/java/com/polimi/ckb/battleService/service/GitService.java
@@ -1,9 +1,17 @@
 package com.polimi.ckb.battleService.service;
 
+import com.polimi.ckb.battleService.dto.CreateBattleDto;
+import com.polimi.ckb.battleService.dto.CreatedBattleDto;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.IOException;
 
 public interface GitService {
-    void uploadYamlFileForNotifications(final String repositoryUrl) throws GitAPIException, IOException;
+    void uploadSetupFiles(final String repositoryUrl, final String repoName) throws GitAPIException, IOException;
+
+    String createGitHubRepository(CreateBattleDto createBattleDto);
+
+    void createSecrets(final CreatedBattleDto dto, final String sonarCloudProjectToken) throws IOException;
+
+    void createSonarCloudProject(final CreateBattleDto dto) throws IOException;
 }
