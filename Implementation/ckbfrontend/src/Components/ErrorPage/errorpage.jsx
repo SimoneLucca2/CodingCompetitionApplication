7498bd9dc5312773b899297c9f458c2544e773c9
ferroo01 <alessandro.feraboli@mail.polimi.it>
2024-01-29 16:50:45 +0100

    Remove large file from tracking

--- Implementation/ckbfrontend/src/Components/ErrorPage/errorpage.jsx
+++ Implementation/ckbfrontend/src/Components/ErrorPage/errorpage.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
 import './errorpage.css';
-import errorImage from './errorimmage.png'; // Assume you have an error-themed image
+import errorImage from './errorimage.png';
 
 function ErrorPage({ message }) {
+    const navigate = useNavigate(); // Hook per la navigazione
+
+    // Animations
+    const containerVariants = {
+        hidden: { opacity: 0, scale: 0.8 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: { delay: 0.3, duration: 0.5 }
+        },
+    };
+
+    // Funzione per tornare indietro
+    const goBack = () => navigate(-1); // -1 per tornare alla pagina precedente
+
     return (
-        <div className="error-page">
+        <motion.div
+            className="error-page"
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+        >
             <img src={errorImage} alt="Error" />
             <h1>Oops! Something went wrong...</h1>
             <p>{message || "We're sorry, but something went wrong. Please try again later."}</p>
-            <button onClick={() => window.location.href = '/'}>Go Back Home</button>
-        </div>
+            <button onClick={goBack}>Go Back</button>
+        </motion.div>
     );
 }
 
---
+++ Implementation/ckbfrontend/src/Components/SuccessPage/SuccessPage.jsx
@@ -0,0 +1,27 @@
+// SuccessPage.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaCheckCircle } from 'react-icons/fa';
+import { useSpring, animated } from 'react-spring';
+import './SuccessPage.css';
+
+const SuccessPage = () => {
+    const navigate = useNavigate();
+    const fade = useSpring({ from: { opacity: 0 }, opacity: 1, delay: 300 });
+    const buttonAnimation = useSpring({ from: { scale: 0.8 }, to: { scale: 1 }, loop: { reverse: true } });
+
+    return (
+        <div className="success-container">
+            <animated.div style={fade} className="success-card">
+                <animated.div style={fade}>
+                    <FaCheckCircle className="success-icon"/>
+                    <h1>Action Completed!</h1>
+                    <p>Your process has been successfully completed.</p>
+                </animated.div>
+                <animated.button style={buttonAnimation} onClick={() => navigate(-1)}>Go Back</animated.button>
+            </animated.div>
+        </div>
+    );
+}
+
+export default SuccessPage;
