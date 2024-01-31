b5cf4ba467d6829718c1c3ff1ffc57ca6659aeb2
MarcoF17 <marco1.filippini@mail.polimi.it>
2024-01-31 16:25:52 +0100

    changes

--- Implementation/battle-service/src/main/java/com/polimi/ckb/battleService/BattleStarter.java
+++ Implementation/battle-service/src/main/java/com/polimi/ckb/battleService/BattleStarter.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BattleStarter {
     public static void main(String[] args) {
         SpringApplication.run(BattleStarter.class, args);
